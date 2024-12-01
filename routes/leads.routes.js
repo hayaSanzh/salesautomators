d2b7d4406d919c68708822a66946ba841d5a982c
Sanzhar Nurmukhanbet <n.sanzhar06@gmail.com>
2024-12-01 12:17:05 +0500

    Adding PATCH and DELETE methods

--- routes/leads.routes.js
+++ routes/leads.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getLeads, getLead, postLead } = require('../controllers/leads.controller.js');
+const { getLeads, getLead, postLead, deleteLead, updateLead } = require('../controllers/leads.controller.js');
 
 router.get('/', getLeads);
 router.get('/:id', getLead);
 router.post('/', postLead);
+router.delete('/:id', deleteLead);
+router.patch('/:id', updateLead);
 
 module.exports = router;
